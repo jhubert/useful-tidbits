add233f2bd2ca1bd7a302469fafa5d144c336d15
jhubert <jhubert@gmail.com>
2012-10-15 14:14:19 -0700

    Added the capture_stdout file
    This is very useful for testing Ruby scripts that output to STDOUT

---
+++ capture_stdout.rb
@@ -0,0 +1,13 @@
+# Source: http://thinkingdigitally.com/archive/capturing-output-from-puts-in-ruby/
+require 'stringio'
+
+module Kernel
+  def capture_stdout
+    out = StringIO.new
+    $stdout = out
+    yield
+    return out
+  ensure
+    $stdout = STDOUT
+  end
+end
